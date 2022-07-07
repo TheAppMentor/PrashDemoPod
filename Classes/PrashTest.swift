7920f7d34cfa003b211993096551c9c649492644
Prashanth <prashanth.moorthy@gmail.com>
2022-07-07 14:48:29 +0530

    Setup Prash Demo Pod

---
+++ Classes/PrashTest.swift
@@ -0,0 +1,17 @@
+//
+//  PrashTest.swift
+//  PrashDemoPod
+//
+//  Created by Prashanth Moorthy on 07/07/22.
+//
+
+import Foundation
+
+public class PrashTestClass {
+    
+    public func someTestFunc() -> String {
+        print("Test file running here.. ")
+        return "Hello There ... Hello From Prash Demo Pod"
+    }
+
+}
---
+++ Example/Pods/Target Support Files/PrashDemoPod/PrashDemoPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PrashDemoPod : NSObject
+@end
+@implementation PodsDummy_PrashDemoPod
+@end
